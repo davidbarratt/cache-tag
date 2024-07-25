74887cfbd7445409d0e6fb3080c6c58b02379bcc
David Barratt <david@davidwbarratt.com>
2024-07-24 22:27:30 -0400

    Fix todo

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,9 @@ export default {
 	async fetch(request) {
 		const response = await fetch(request);
 
-		// @TODO When there is a cache MISS *and* the response has tags, push an insert into the queue.
+		/**
+		 * @todo When there is a cache MISS *and* the response has tags, push an insert into the queue.
+		 */
 		console.log("URL", response.url);
 		console.log("CACHE", response.headers.get("CF-Cache-Status"));
 
