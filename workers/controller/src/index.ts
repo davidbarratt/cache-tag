ebe8c61970e60cd54294823cd1c18d9c3b9e706d
David Barratt <david@davidwbarratt.com>
2024-08-06 22:08:10 -0400

    Forward all requests to the controller

--- workers/controller/src/index.ts
+++ workers/controller/src/index.ts
@@ -1,6 +1,13 @@
 import Cloudflare from "cloudflare";
-import { WorkerEntrypoint } from "cloudflare:workers";
+import rawParams from "tracking-query-params-registry/_data/params.csv";
 import { z } from "zod";
+
+const params = new Set<string>(
+	rawParams.split("\n").map((line) => {
+		const end = line.indexOf(",");
+		return line.substring(0, end).trim();
+	}),
+);
 
 interface CaptureBody {
 	url: string;
@@ -9,11 +16,6 @@ interface CaptureBody {
 }
 
 const Purge = z.object({
-	tags: z.array(z.string()),
-});
-
-const Capture = z.object({
-	url: z.string().url(),
 	tags: z.array(z.string()),
 });
 
@@ -75,6 +77,7 @@ async function handlePurgeRequest(request: Request, env: Env) {
 
 	const { tags } = Purge.parse(await request.json());
 
+	// If no zone is present, then all zones will be purged.
 	const zone = request.headers.get("CF-Worker") ?? undefined;
 
 	const messages = tags.map<MessageSendRequest<PurgeBody>>((tag) => ({
@@ -82,7 +85,7 @@ async function handlePurgeRequest(request: Request, env: Env) {
 			tag,
 			zone,
 		},
-		contentType: "json",
+		contentType: "text",
 	}));
 
 	// sendBatch only allows for a maximum of 100 messages.
@@ -96,76 +99,57 @@ async function handlePurgeRequest(request: Request, env: Env) {
 	return new Response("", { status: 202 });
 }
 
-async function handleCaptureRequest(request: Request, env: Env) {
-	const zone = request.headers.get("CF-Worker");
-	if (!zone) {
-		return Response.json(
-			{
-				error: "Missing CF-Worker header",
-			},
-			{ status: 400 },
-		);
-	}
-
-	const auth = request.headers.get("Authorization");
-	if (!auth) {
-		return Response.json(
-			{
-				error: "Missing Authorization header",
-			},
-			{ status: 401 },
-		);
-	}
-	const [scheme, token] = auth.split(" ");
-	if (scheme !== "Bearer") {
-		return Response.json(
-			{
-				error: "Authorization scheme is not Bearer",
-			},
-			{ status: 401 },
-		);
-	}
-
-	// Needs at least `Cache Purge:Purge, Zone:Read" permissions.
-	if (token !== env.API_TOKEN) {
-		return Response.json(
-			{
-				error: "Provided token does not match the `API_TOKEN` secret.",
-			},
-			{ status: 401 },
-		);
-	}
-
-	const { url, tags } = Capture.parse(await request.json());
-
-	const capture: CaptureBody = {
-		url,
-		tags,
-		zone,
-	};
-
-	await env.CACHE_CAPTURE.send(capture, { contentType: "json" });
-
-	return new Response("", { status: 202 });
-}
-
 export default {
 	async fetch(request, env) {
-		if (request.method !== "POST") {
-			return new Response("", { status: 400 });
-		}
-
 		// Remove any tracking params to increase the cache hit rate.
 		const url = new URL(request.url);
 
-		if (url.pathname === "/purge") {
+		if (request.method === "POST" && url.pathname === "/.cloudflare/purge") {
 			return handlePurgeRequest(request, env);
 		}
 
-		if (url.pathname === "/capture") {
-			return handleCaptureRequest(request, env);
+		if (url.searchParams.size > 0) {
+			for (const key of url.searchParams.keys()) {
+				if (params.has(key)) {
+					url.searchParams.delete(key);
+				}
+			}
 		}
 
-		return new Response("", { status: 404 });
+		const response = await fetch(url, request);
+
+		// If there is no zone on the request,
+		// then we wont know how to purge the response later.
+		const zone = request.headers.get("CF-Worker");
+		if (!zone) {
+			return response;
+		}
+
+		if (response.headers.get("CF-Cache-Status") !== "MISS") {
+			return response;
+		}
+
+		if (!response.headers.has("X-Cache-Tag")) {
+			return response;
+		}
+
+		const rawTags = response.headers.get("X-Cache-Tag");
+
+		if (!rawTags) {
+			return response;
+		}
+
+		// A set here removes any duplicates.
+		const tags = new Set<string>(rawTags.split(",").map((tag) => tag.trim()));
+
+		const capture: CaptureBody = {
+			url: response.url,
+			zone,
+			tags: Array.from(tags),
+		};
+
+		await env.CACHE_CAPTURE.send(capture, { contentType: "json" });
+
+		return response;
 	},
 } satisfies ExportedHandler<Env>;
