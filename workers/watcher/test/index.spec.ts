b69e1822da1972dd1fc0dd477c4edd0c95bf82cd
David Barratt <david@davidwbarratt.com>
2024-08-08 22:28:34 -0400

    Add a license

--- workers/watcher/test/index.spec.ts
+++ workers/watcher/test/index.spec.ts
@@ -12,79 +12,12 @@ afterEach(() => {
 	fetchMock.assertNoPendingInterceptors();
 });
 
-it("adds cache tags to the capture queue", async () => {
-	const sendSpy = vi.spyOn(env.CACHE_CAPTURE, "send").mockResolvedValue();
-
-	fetchMock
-		.get("https://example.com")
-		.intercept({ path: "/" })
-		.reply(200, "", {
-			headers: {
-				"CF-Cache-Status": "MISS",
-				"X-Cache-Tag": "test",
-			},
-		});
+it("forwards every request to the controller", async () => {
+	const sendSpy = vi
+		.spyOn(env.CACHE_CONTROLLER, "fetch")
+		.mockResolvedValue(new Response());
 
 	await SELF.fetch("https://example.com");
 
-	expect(sendSpy).toBeCalledWith(
-		{
-			url: "", // I hate that this is empty, but I don't see a good way to mock it.
-			tags: ["test"],
-		},
-		{ contentType: "json" },
-	);
-});
-
-it("passes when CF-Cache-Status is something other than MISS", async () => {
-	const sendSpy = vi.spyOn(env.CACHE_CAPTURE, "send").mockResolvedValue();
-
-	fetchMock
-		.get("https://example.com")
-		.intercept({ path: "/" })
-		.reply(200, "", {
-			headers: {
-				"CF-Cache-Status": "DYNAMIC",
-				"X-Cache-Tag": "test",
-			},
-		});
-
-	await SELF.fetch("https://example.com");
-
-	expect(sendSpy).not.toBeCalled();
-});
-
-it("passes when X-Cache-Tag is not set", async () => {
-	const sendSpy = vi.spyOn(env.CACHE_CAPTURE, "send").mockResolvedValue();
-
-	fetchMock
-		.get("https://example.com")
-		.intercept({ path: "/" })
-		.reply(200, "", {
-			headers: {
-				"CF-Cache-Status": "DYNAMIC",
-			},
-		});
-
-	await SELF.fetch("https://example.com");
-
-	expect(sendSpy).not.toBeCalled();
-});
-
-it("passes when X-Cache-Tag is empty", async () => {
-	const sendSpy = vi.spyOn(env.CACHE_CAPTURE, "send").mockResolvedValue();
-
-	fetchMock
-		.get("https://example.com")
-		.intercept({ path: "/" })
-		.reply(200, "", {
-			headers: {
-				"CF-Cache-Status": "DYNAMIC",
-				"X-Cache-Tag": "",
-			},
-		});
-
-	await SELF.fetch("https://example.com");
-
-	expect(sendSpy).not.toBeCalled();
+	expect(sendSpy).toBeCalled();
 });
