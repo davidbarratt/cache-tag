1e3c00719e46b4aaa1a8dc2e51b6c8abb9750984
David Barratt <david@davidwbarratt.com>
2024-08-29 23:50:07 -0400

    Fix linting

--- workers/watcher/test/index.spec.ts
+++ workers/watcher/test/index.spec.ts
@@ -1,23 +1,25 @@
-import { SELF, env, fetchMock } from "cloudflare:test";
-import { beforeAll, afterEach, it, expect, vi } from "vitest";
+import { SELF, fetchMock } from "cloudflare:test";
+import { beforeAll, afterEach, it, expect } from "vitest";
 
 // For now, you'll need to do something like this to get a correctly-typed
 // `Request` to pass to `worker.fetch()`.
 // const IncomingRequest = Request<unknown, IncomingRequestCfProperties>;
 
 beforeAll(() => {
-	fetchMock.activate();
+  fetchMock.activate();
 });
 afterEach(() => {
-	fetchMock.assertNoPendingInterceptors();
+  fetchMock.assertNoPendingInterceptors();
 });
 
 it("forwards every request to the controller", async () => {
-	const sendSpy = vi
-		.spyOn(env.CACHE_CONTROLLER, "fetch")
-		.mockResolvedValue(new Response());
+  // Mock the first request to `https://example.com`
+  fetchMock
+    .get("https://example.com")
+    .intercept({ path: "/" })
+    .reply(200, "body");
 
-	await SELF.fetch("https://example.com");
+  const response = await SELF.fetch("https://example.com");
 
-	expect(sendSpy).toBeCalled();
+  expect(await response.text()).toBe("body");
 });
